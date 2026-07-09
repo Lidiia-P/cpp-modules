a883ea5b6ccd8cb8bc672e906a2dea6d88d9891c
Lidiia Perekhrest <lidiia.perekhrest@gmail.com>
2026-07-09 14:12:06 +0200

    year check in data validation

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -151,6 +151,8 @@ bool	BitcoinExchange::isValidDate(const std::string &date) const {
 	int month = std::atoi(date.substr(5, 2).c_str());
 	int day = std::atoi(date.substr(8, 2).c_str());
 
+	if (year < 1 || year > 9999)
+		return false;
 	if (month < 1 || month > 12)
 		return false;
 	if (day < 1)
